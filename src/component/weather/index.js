98c3d92a2ee1c3ed231548f7504e0b092dfa0abc
yusungsamuel <ssyyu@ucdavis.edu>
2020-05-24 12:57:24 -0700

    render data on weather component

--- src/component/weather/index.js
+++ src/component/weather/index.js
@@ -4,31 +4,54 @@ import API from "../../utilities/API";
 import axios from "axios";
 
 export default function Weather() {
-    const [lat, setLat] = useState("37.7749");
-    const [long, setLong] = useState("-122.4194");
-
+    const [lat, setLat] = useState(37.7749);
+    const [long, setLong] = useState(-122.4194);
+    const [data, setData] = useState(null)
     useEffect(() => {
-        let cancel
-
-        API.weather(lat, long)
-            .then(response => {
-                console.log(response.data)
-                new axios.CancelToken(c => cancel = c)
-                return () => cancel()
-            })
-
-
-        navigator.geolocation.getCurrentPosition(success);
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(success);
+        }
 
         function success(position){
-            console.log(position.coords)
+            setLat(position.coords.latitude)
+            setLong(position.coords.longitude)
         }
 
     }, [])
+    
+
+    useEffect(()=>{
+        let cancel            
+        API.weather(lat, long)
+        .then(response => {
+            console.log(response.data.current)
+            setData(response.data.current)
+            new axios.CancelToken(c => cancel = c)
+            return () => cancel()
+        })
+    },[long||lat])
+
+    // useEffect(()=>{
+    //     let cancel            
+    //     API.weather(lat, long)
+    //     .then(response => {
+    //         console.log(response.data)
+    //         setData(response.data)
+    //         new axios.CancelToken(c => cancel = c)
+    //         return () => cancel()
+    //     })
+    // }, [])
 
     return (
-        <div>
-            Weather
-        </div>
+    data === null? 
+    <div>The weather data for your location is currently unavailable. Please try again later</div>
+    :
+    <div>
+        <p>{data.weather_descriptions}</p>
+        <img src={data.weather_icons[0]} alt="weather icon"></img>
+        <p>{data.temperature}°C</p>
+        <p>Humidity: {data.humidity}%</p>
+    <p>Cloud Cover: {data.cloudcover}%</p>
+    </div>
     )
 }
