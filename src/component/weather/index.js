f6af2d0fc9abe4a7269b3049dd857fdc081d1575
yusungsamuel <ssyyu@ucdavis.edu>
2020-06-25 11:39:57 -0700

    modified weather component to calculate time forcast. Still need to fix algo to calculate correctly

--- src/component/weather/index.js
+++ src/component/weather/index.js
@@ -8,7 +8,7 @@ export default function Weather() {
     const [long, setLong] = useState(-122.4194);
     const [daily, setDaily] = useState(null)
     const [hourly, setHourly] = useState(null)
-
+    const [date, setDate] = useState(new Date())
     useEffect(() => {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(success);
@@ -34,28 +34,59 @@ export default function Weather() {
             })
     }, [long || lat])
 
-    const date = new Date()
-    
-    console.log(date)
-    return (        
+    const renderSwitch = (num) => {
+        switch (num) {
+            case 0:
+                return "Sunday";
+            case 1:
+                return "Monday";
+
+            case 2:
+                return "Tuesday";
+
+            case 3:
+                return "Wednesday";
+
+            case 4:
+                return "Thursday";
+
+            case 5:
+                return "Friday";
+
+            case 6:
+                return "Saturday";
+        }
+    }
+
+    return (
         <div className="weather-container">
             {hourly === null ?
                 <div>The weather data for your location is currently unavailable. Please try again later</div>
                 :
                 <div>
-                    <div className="topSection">  
+                    <div className="topSection">
                         <p className="city">(City Name)</p>
                         <p className="description">{daily[0].weather[0].description}</p>
                         <p className="temperature">{Math.round(daily[0].temp.day)}°</p>
-                        <span className="date">(Date): Today</span> <span className="range">({Math.round(daily[0].temp.max)} | {Math.round(daily[0].temp.min)})</span>
+                        <span className="date">{renderSwitch(date.getDay())} Today</span> <span className="range">{Math.round(daily[0].temp.max)}{Math.round(daily[0].temp.min)}</span>
                     </div>
                     <div className="scroll">
                         {
-                            hourly.map((h, i )=>{
+                            hourly.map((h, i) => {
+
+                                if ((date.getHours() + i) % 24 === 12) {
+                                    return (
+                                        <div key={i}>
+                                            <div>{i > 0 ? ((date.getHours() + i) % 24 >= 12 ? (date.getHours() + i - 12) % 23 + "PM" : (date.getHours() + i) % 24 + "AM") : "Now"}</div>
+                                            <div><img src={" http://openweathermap.org/img/wn/" + h.weather[0].icon + "@2x.png"} /></div>
+                                        </div>
+                                    )
+                                }
+
                                 return (
                                     <div key={i}>
-                                        <div>(time)</div>
-                                        <div><img src={" http://openweathermap.org/img/wn/" + h.weather[0].icon + "@2x.png"}/></div>
+                                        <div>{i > 0 ? ((date.getHours() + i) % 24 >= 12 ? (date.getHours() + i - 12) % 23 + "PM" : (date.getHours() + i) % 24 + "AM") : "Now"}</div>
+                                        <div><img src={" http://openweathermap.org/img/wn/" + h.weather[0].icon + "@2x.png"} /></div>
                                     </div>
                                 )
                             })
