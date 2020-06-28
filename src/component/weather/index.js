1fb54beb32a4108a7ab3c5899c6c4528824ad610
Your Name <you@example.com>
2020-06-27 21:25:34 -0700

    add week forecast

--- src/component/weather/index.js
+++ src/component/weather/index.js
@@ -58,45 +58,59 @@ export default function Weather() {
     }
 
     const calculateHours = (num) => {
-        if(num === 0){
+        if (num === 0) {
             return "12AM"
         }
-        else if(num< 12){
+        else if (num < 12) {
             return num + "AM"
         }
         else {
-            return num%12 === 0 ?  "12PM" : num%12+"PM"
+            return num % 12 === 0 ? "12PM" : num % 12 + "PM"
         }
     }
-
-
     return (
+
         <div className="weather-container">
             {hourly === null ?
                 <div>The weather data for your location is currently unavailable. Please try again later</div>
                 :
                 <div>
+                    {console.log(date)}
                     <div className="topSection">
                         <p className="city">(City Name)</p>
                         <p className="description">{daily[0].weather[0].description}</p>
                         <p className="temperature">{Math.round(daily[0].temp.day)}°</p>
-                        <span className="date">{renderSwitch(date.getDay())} Today</span> <span className="range">{Math.round(daily[0].temp.max)}{Math.round(daily[0].temp.min)}</span>
+                        <span className="date">{renderSwitch(date.getDay())} Today</span> <span className="range">{Math.round(daily[0].temp.max)} {Math.round(daily[0].temp.min)}</span>
                     </div>
                     <div className="scroll">
                         {
                             hourly.map((h, i) => {
-                                if(i>0){
-                                    date.setHours(date.getHours()+1)
+                                if (i > 0) {
+                                    date.setHours(date.getHours() + 1)
                                 }
                                 return (
                                     <div key={i}>
-                                        <div>{i>0 ? calculateHours(date.getHours()): "Now"}</div>
+                                        <div>{i > 0 ? calculateHours(date.getHours()) : "Now"}</div>
                                         <div><img src={" http://openweathermap.org/img/wn/" + h.weather[0].icon + "@2x.png"} /></div>
                                     </div>
                                 )
                             })
                         }
                     </div>
+                    {
+                        daily.map((d, i) => {
+                            if (i === 0) return
+                            let day = new Date();
+                            day.setDate(day.getDate() + i)
+                            return (
+                                <div className="week-forecast">
+                                    <div className="weekday">{renderSwitch(day.getDay())}</div>
+                                    <div className="icon"><img src={" http://openweathermap.org/img/wn/" + d["weather"][0]["icon"] + "@2x.png"} /></div>
+                                    <div><span className="range">{Math.round(d.temp.max)} {Math.round(d.temp.min)}</span></div>
+                                </div>
+                            )
+                        })
+                    }
 
                 </div>
             }
